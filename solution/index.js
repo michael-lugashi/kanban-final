460c1ac6e79cdab0cdd4e69e73dd4931ed13265d
michael-lugashi <89575620+michael-lugashi@users.noreply.github.com>
2021-09-22 21:49:48 +0300

    Local Storage
    I made it so local Storage is fully functional and passes the tests. Every time the page reloads, if there is localStorage, I turn it into an object and loop through it, and add all the elements back  on the page. Every time I add an element I update the listStorage object and I update the local Storage.

--- solution/index.js
+++ solution/index.js
@@ -1,34 +1,67 @@
 'use strict'
-const tasksContainer = document.getElementById('tasks-container')
+
+/* DOM Events */
+const tasksContainer = document.getElementById('tasks-container') //global variable that never changes
 tasksContainer.onclick = chooseButton
 
-function chooseButton(event){
-    // one click event works on all three buttons and only buttons
-    const button = event.target
-    if (button.tagName !== 'BUTTON') return
+// I initialize a storage object
+let listStorage = {
+  todo: [],
+  'in-progress': [],
+  done: [],
+}
 
-    // gets the section and input that are relavent to the button
-    const section = button.closest('section')
-    const input = document.querySelector(`#${section.id} > input`)
+/* Local Storage */
+getTasksFromLocalStorage()
 
-    // I don't allow the user to enter an empt input
-    if (!input.value) {
-        alert(`You can't submit an empty task`)
-        return
+function getTasksFromLocalStorage() {
+  const localStorageLists = localStorage.getItem('tasks')
+
+  if (localStorageLists) {
+    // I make the storage object equal to local storage if local storage has values
+    listStorage = JSON.parse(localStorageLists)
+
+    // I place all the data from local storage into the DOM
+    for (const list in listStorage) {
+      const currentList = tasksContainer.querySelector(`#${list} > ul`)
+      listStorage[list].forEach((listItemText) => {
+        currentList.append(createListElement('li', listItemText, 'task'))
+      })
     }
-    
-    // I select the list within the section and append a list element
-    const list = document.querySelector(`#${section.id} > ul`)
-    list.append(createListElement('li', input.value, 'listItem'))
+  }
+}
 
-    // I clear the input
-    input.value = ''
+function chooseButton(event) {
+  // one click event works on all three buttons and only on buttons
+  const button = event.target
+  if (button.tagName !== 'BUTTON') return
 
+  // gets the section and input that are relavent to the button
+  const section = button.closest('section')
+  const input = document.querySelector(`#${section.id} > input`)
+
+  // I don't allow the user to enter an empt input
+  if (!input.value) {
+    alert(`You can't submit an empty task`)
+    return
+  }
+
+  // I select the list within the section and append a list element
+  const list = document.querySelector(`#${section.id} > ul`)
+  list.append(createListElement('li', input.value, 'task'))
+
+  
+  // I update the local storage
+  listStorage[section.id].push(input.value)
+  localStorage.setItem('tasks', JSON.stringify(listStorage))
+
+  // I clear the input
+  input.value = ''
 }
 
 function createListElement(tagname, text, cls) {
-    const newListItem = document.createElement(tagname)
-    newListItem.textContent = text
-    newListItem.classList.add(cls)
-    return newListItem
+  const newListItem = document.createElement(tagname)
+  newListItem.textContent = text
+  newListItem.classList.add(cls)
+  return newListItem
 }
